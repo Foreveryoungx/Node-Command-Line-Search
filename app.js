51b13b8cb88743f4200622bd8447fe04f0d4e161
Trey Allen <trevon.allen1994@gmail.com>
2022-01-30 23:35:46 -0600

    Finish up error handle to identify the error code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 //require HTTPS module
 const https = require('https');
+const http = require("http");
 
 // Print Error Messages
 function printError(error){
@@ -17,22 +18,28 @@ function getProfile(username) {
     try {
 //connect to API URL
         const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-            let body = "";
-            // Read the data
-            response.on('data', data => {
-                body += data.toString();
-            });
-            // Parse the data
-            response.on('end', () => {
-                try {
-                    const profile = JSON.parse(body);
-                    // Print out data
-                    printMessage(username, profile.badges.length, profile.points.JavaScript)
-                } catch (error) {
-                    printError(error);
-                }
-            })
-
+            if(response.statusCode === 200) {
+                let body = "";
+                // Read the data
+                response.on('data', data => {
+                    body += data.toString();
+                });
+                // Parse the data
+                response.on('end', () => {
+                    try {
+                        const profile = JSON.parse(body);
+                        // Print out data
+                        printMessage(username, profile.badges.length, profile.points.JavaScript)
+                    } catch (error) {
+                        printError(error);
+                    }
+                })
+            }else {
+                const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`
+                //create a new error object if code is not 200 ok
+                const statusCodeError = new Error(message);
+                printError(statusCodeError)
+            }
 
         })
         //add error handle
